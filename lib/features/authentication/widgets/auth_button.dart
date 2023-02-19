49cb503d39284c4e1ddcdfc390b72367a10c9e6f
오태현 <jeffrey@kai-i.com>
2023-02-19 19:25:04 +0900

    FontAwesome 추가, AuthButton 위젯 완성

--- lib/features/authentication/widgets/auth_button.dart
+++ lib/features/authentication/widgets/auth_button.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class AuthButton extends StatelessWidget {
   final String text;
+  final FaIcon icon;
 
   const AuthButton({
     super.key,
     required this.text,
+    required this.icon,
   });
 
   @override
@@ -15,22 +18,31 @@ class AuthButton extends StatelessWidget {
       // FractionallySizedBox - 부모의 영역에 맞추는 위젯
       widthFactor: 1, // 비율
       child: Container(
-        padding: const EdgeInsets.symmetric(
-          vertical: Sizes.size14,
-        ),
+        padding: const EdgeInsets.all(Sizes.size14),
         decoration: BoxDecoration(
           border: Border.all(
             color: Colors.grey.shade300,
             width: Sizes.size1,
           ),
         ),
-        child: Text(
-          text,
-          style: const TextStyle(
-            fontSize: Sizes.size16,
-            fontWeight: FontWeight.w600,
-          ),
-          textAlign: TextAlign.center,
+        child: Stack(
+          // Stack - 하나씩 위젯을 쌓아주는 위젯
+          alignment: Alignment.center,
+          children: [
+            Align(
+              // Align - Stack 안에서 위치를 조정시켜주는 위젯
+              alignment: Alignment.centerLeft,
+              child: icon,
+            ),
+            Text(
+              text,
+              style: const TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
