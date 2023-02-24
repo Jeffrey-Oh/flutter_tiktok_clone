8fbcc7b7c30a10c03968ce3787172a36c9f960d3
오태현 <jeffrey@kai-i.com>
2023-02-24 17:51:21 +0900

    BottomNavigationBar 샘플 코드 추가

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    const Center(
+      child: Text('Home'),
+    ),
+    const Center(
+      child: Text('Search'),
+    ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        onTap: _onTap,
+        currentIndex: _selectedIndex,
+        // selectedItemColor: Theme.of(context).primaryColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.house,
+            ),
+            label: "house",
+            tooltip: "hello",
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+            ),
+            label: "house",
+            tooltip: "hi",
+            backgroundColor: Colors.blue,
+          ),
+        ],
+      ),
+    );
   }
 }
