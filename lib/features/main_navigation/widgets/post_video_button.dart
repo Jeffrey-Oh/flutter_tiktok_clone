8162ad8b15b21d6134bec141a018b16bf1835bea
오태현 <jeffrey@kai-i.com>
2023-02-27 16:36:25 +0900

    post_video_button 누락으로 추가

---
+++ lib/features/main_navigation/widgets/post_video_button.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class PostVideoButton extends StatelessWidget {
+  const PostVideoButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip
+          .none, // Stack 은 쌓기 때문에 최상위 위젯만 보고 나머지는 Clip 으로 잘려있다. 따라서 포지션으로 이동 후에도 보이기 위해서는 Clip 처리를 none 해야함
+      children: [
+        Positioned(
+          // Stack 에 쌓인 위젯을 강제로 옮기게 해줌
+          right: 20,
+          child: Container(
+            height: 30,
+            width: 25,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: const Color(0xff61D4F0),
+              borderRadius: BorderRadius.circular(
+                Sizes.size8,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 20,
+          child: Container(
+            height: 30,
+            width: 25,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(
+                Sizes.size8,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          height: 30,
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size12,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              Sizes.size6,
+            ),
+          ),
+          child: const Center(
+            child: FaIcon(
+              FontAwesomeIcons.plus,
+              color: Colors.black,
+              size: 18,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
