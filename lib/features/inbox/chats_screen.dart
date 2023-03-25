de1ae68d8765225e19d925d49519e49ef1634a02
오태현 <jeffrey@kai-i.com>
2023-03-25 16:57:42 +0900

    AnimatedList 추가 및 ListItem 추가하는 방법

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -10,6 +10,20 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(milliseconds: 300),
+      );
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +32,7 @@ class _ChatScreenState extends State<ChatScreen> {
         title: const Text('Direct messages'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(
               FontAwesomeIcons.plus,
               size: Sizes.size18,
@@ -26,38 +40,46 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(
           vertical: Sizes.size10,
         ),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 30,
-              foregroundImage: NetworkImage(
-                  "https://avatars.githubusercontent.com/u/45170847?v=4"),
-              child: Text('제프리'),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  'yeeun',
-                  style: TextStyle(fontWeight: FontWeight.w500),
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 30,
+                  foregroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/45170847?v=4"),
+                  child: Text('제프리'),
                 ),
-                Text(
-                  "4:30 PM",
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontSize: Sizes.size12,
-                  ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'yeeun ($index)',
+                      style: const TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "4:30 PM",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: Sizes.size12,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+                subtitle: const Text('안뇽'),
+              ),
             ),
-            subtitle: const Text('안뇽'),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
