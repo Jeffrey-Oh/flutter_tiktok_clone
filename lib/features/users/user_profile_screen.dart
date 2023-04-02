0e6d3cf2eeb685762110f791813b8991a57f6f0c
오태현 <jeffrey@kai-i.com>
2023-04-02 15:59:55 +0900

    user profile screen 마무리
    - 코드 첼린지 2개 추가
    - Follow 버튼 우측에 버튼 2개 추가 (유튜브, 드롭다운)
    - Grid Item 에 Stack 사용하여 View Count 표시 및 아이콘 추가

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/users/widgets/persistent_tabbar.dart';
 import 'package:tiktok_clone/features/users/widgets/user_info.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -16,258 +17,177 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            title: const Text('제프리'),
-            actions: [
-              IconButton(
-                onPressed: () {},
-                icon: const FaIcon(
-                  FontAwesomeIcons.gear,
-                  size: Sizes.size20,
+    return SafeArea(
+      child: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                title: const Text('제프리'),
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const FaIcon(
+                      FontAwesomeIcons.gear,
+                      size: Sizes.size20,
+                    ),
+                  ),
+                ],
+              ),
+              SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    const CircleAvatar(
+                      radius: 50,
+                      foregroundColor: Colors.blue,
+                      foregroundImage: NetworkImage(
+                          "https://avatars.githubusercontent.com/u/45170847?v=4"),
+                      child: Text("제프리"),
+                    ),
+                    Gaps.v20,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "@jeffrey",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: Sizes.size14,
+                          ),
+                        ),
+                        if (isOfficialUser) Gaps.h5,
+                        if (isOfficialUser)
+                          FaIcon(
+                            FontAwesomeIcons.solidCircleCheck,
+                            size: Sizes.size16,
+                            color: Colors.blue.shade300,
+                          ),
+                      ],
+                    ),
+                    Gaps.v24,
+                    SizedBox(
+                      height: Sizes.size48,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const UserInfo(count: "97", title: "Following"),
+                          VerticalDivider(
+                            width: Sizes.size32,
+                            thickness: Sizes.size1,
+                            color: Colors.grey.shade400,
+                            indent: Sizes.size14,
+                            endIndent: Sizes.size14,
+                          ),
+                          const UserInfo(count: "10M", title: "Followers"),
+                          VerticalDivider(
+                            width: Sizes.size32,
+                            thickness: Sizes.size1,
+                            color: Colors.grey.shade400,
+                            indent: Sizes.size14,
+                            endIndent: Sizes.size14,
+                          ),
+                          const UserInfo(count: "194.3M", title: "Likes"),
+                        ],
+                      ),
+                    ),
+                    Gaps.v14,
+                    // FractionallySizedBox - 항상 부모의 width, height 를 기준으로 한다
+                    FractionallySizedBox(
+                      widthFactor: 0.33,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: Sizes.size12,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(
+                              Sizes.size4,
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          'Follow',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    Gaps.v14,
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: Sizes.size32,
+                      ),
+                      child: Text(
+                        "Hello Flutter !!! This is Tiktok Clone",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Gaps.v14,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        FaIcon(
+                          FontAwesomeIcons.link,
+                          size: Sizes.size12,
+                        ),
+                        Gaps.h4,
+                        Text(
+                          "https://github.com/Jeffrey-Oh",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Gaps.v20,
+                  ],
+                ),
+              ),
+              SliverPersistentHeader(
+                delegate: PersistentTabBar(),
+                pinned: true,
+              ),
+            ];
+          },
+          body: TabBarView(
+            children: [
+              GridView.builder(
+                keyboardDismissBehavior:
+                    ScrollViewKeyboardDismissBehavior.onDrag,
+                padding: EdgeInsets.zero,
+                itemCount: 20,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: Sizes.size2,
+                  mainAxisSpacing: Sizes.size2,
+                  childAspectRatio: 9 / 14,
+                ),
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 9 / 14,
+                      child: FadeInImage.assetNetwork(
+                          fit: BoxFit.cover,
+                          placeholder: "assets/images/placeholder.jpg",
+                          image:
+                              "https://images.unsplash.com/photo-1598373182133-52452f7691ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"),
+                    ),
+                  ],
+                ),
+              ),
+              const Center(
+                child: Text(
+                  "Page two",
                 ),
               ),
             ],
           ),
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                const CircleAvatar(
-                  radius: 50,
-                  foregroundColor: Colors.blue,
-                  foregroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/45170847?v=4"),
-                  child: Text("제프리"),
-                ),
-                Gaps.v20,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "@jeffrey",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: Sizes.size14,
-                      ),
-                    ),
-                    if (isOfficialUser) Gaps.h5,
-                    if (isOfficialUser)
-                      FaIcon(
-                        FontAwesomeIcons.solidCircleCheck,
-                        size: Sizes.size16,
-                        color: Colors.blue.shade300,
-                      ),
-                  ],
-                ),
-                Gaps.v24,
-                SizedBox(
-                  height: Sizes.size48,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const UserInfo(count: "97", title: "Following"),
-                      VerticalDivider(
-                        width: Sizes.size32,
-                        thickness: Sizes.size1,
-                        color: Colors.grey.shade400,
-                        indent: Sizes.size14,
-                        endIndent: Sizes.size14,
-                      ),
-                      const UserInfo(count: "10M", title: "Followers"),
-                      VerticalDivider(
-                        width: Sizes.size32,
-                        thickness: Sizes.size1,
-                        color: Colors.grey.shade400,
-                        indent: Sizes.size14,
-                        endIndent: Sizes.size14,
-                      ),
-                      const UserInfo(count: "194.3M", title: "Likes"),
-                    ],
-                  ),
-                ),
-                Gaps.v14,
-                // FractionallySizedBox - 항상 부모의 width, height 를 기준으로 한다
-                FractionallySizedBox(
-                  widthFactor: 0.33,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: Sizes.size12,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(
-                          Sizes.size4,
-                        ),
-                      ),
-                    ),
-                    child: const Text(
-                      'Follow',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-                Gaps.v14,
-                const Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: Sizes.size32,
-                  ),
-                  child: Text(
-                    "Hello Flutter !!! This is Tiktok Clone",
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Gaps.v14,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    FaIcon(
-                      FontAwesomeIcons.link,
-                      size: Sizes.size12,
-                    ),
-                    Gaps.h4,
-                    Text(
-                      "https://github.com/Jeffrey-Oh",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-                Gaps.v5,
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.symmetric(
-                      horizontal: BorderSide(
-                        color: Colors.grey.shade200,
-                        width: 0.5,
-                      ),
-                    ),
-                  ),
-                  child: const TabBar(
-                    indicatorSize: TabBarIndicatorSize.label,
-                    labelPadding: EdgeInsets.symmetric(
-                      vertical: Sizes.size10,
-                    ),
-                    indicatorColor: Colors.black,
-                    labelColor: Colors.black,
-                    tabs: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: Sizes.size20,
-                        ),
-                        child: Icon(Icons.grid_4x4_rounded),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: Sizes.size20,
-                        ),
-                        child: FaIcon(FontAwesomeIcons.heart),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: TabBarView(
-                    children: [
-                      GridView.builder(
-                        keyboardDismissBehavior:
-                            ScrollViewKeyboardDismissBehavior.onDrag,
-                        padding: const EdgeInsets.all(Sizes.size6),
-                        itemCount: 20,
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: Sizes.size10,
-                          mainAxisSpacing: Sizes.size10,
-                          childAspectRatio: 9 / 20,
-                        ),
-                        itemBuilder: (context, index) => Column(
-                          children: [
-                            Container(
-                              clipBehavior: Clip.hardEdge,
-                              decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.circular(Sizes.size4),
-                              ),
-                              child: AspectRatio(
-                                aspectRatio: 9 / 16,
-                                child: FadeInImage.assetNetwork(
-                                    fit: BoxFit.cover,
-                                    placeholder:
-                                        "assets/images/placeholder.jpg",
-                                    image:
-                                        "https://images.unsplash.com/photo-1598373182133-52452f7691ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"),
-                              ),
-                            ),
-                            Gaps.v10,
-                            const Text(
-                              "This is very long caption for my tiktok that im upload just now currently.",
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                              style: TextStyle(
-                                fontSize: Sizes.size18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Gaps.v5,
-                            DefaultTextStyle(
-                              style: TextStyle(
-                                color: Colors.grey.shade600,
-                                fontWeight: FontWeight.w600,
-                              ),
-                              child: Row(
-                                children: [
-                                  const CircleAvatar(
-                                    radius: Sizes.size12,
-                                    backgroundImage: NetworkImage(
-                                        "https://avatars.githubusercontent.com/u/45170847?v=4"),
-                                  ),
-                                  Gaps.h4,
-                                  const Expanded(
-                                    child: Text(
-                                      "My avatar is going to be very long",
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  ),
-                                  Gaps.h4,
-                                  FaIcon(
-                                    FontAwesomeIcons.heart,
-                                    size: Sizes.size16,
-                                    color: Colors.grey.shade600,
-                                  ),
-                                  Gaps.h2,
-                                  const Text(
-                                    "2.5M",
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const Center(
-                        child: Text(
-                          "Page two",
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
